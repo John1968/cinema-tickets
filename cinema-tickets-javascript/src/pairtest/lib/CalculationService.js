3e41bd79793390899aa8a25befbf6a5a5dbecefa
john.ainsworth <john.ainsworth@engineering.digital.dwp.gov.uk>
2023-11-13 18:59:50 +0000

    feat(build-object): create the basic ticket request object Use the array of ticket requests ti get the total number of tickets by type

--- cinema-tickets-javascript/src/pairtest/lib/CalculationService.js
+++ cinema-tickets-javascript/src/pairtest/lib/CalculationService.js
@@ -1,5 +1,5 @@
 import { BASE_RESERVATION_OBJECT } from '../lib/Config';
-import logger from '../lib/logger';
+// import logger from '../lib/logger';
 export default class CalculationService {
 
     getTotalTicketsByType(ticketTypeRequests) {
